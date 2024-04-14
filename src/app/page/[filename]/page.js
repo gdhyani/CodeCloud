ced7662032eafd4be8f90fa8dae5f41b641cb16e
Gaurav Dhyani <gauravdhyani90@gmail.com>
2024-04-14 11:30:32 +0530

    added monaco editor

--- src/app/page/[filename]/page.js
+++ src/app/page/[filename]/page.js
@@ -1,11 +1,17 @@
 "use client";
 import { useRouter } from "next/navigation";
-
+import Editor from "@monaco-editor/react";
 export default function Filepage({ params }) {
     const router = useRouter();
     return (
-        <div>
-            <h1>File Page Current File : {params.filename}</h1>
+        <div className="h-screen">
+            <h1 className="border text-muted-foreground">{params.filename}</h1>
+            <Editor
+                theme="vs-dark"
+                height="90vh"
+                defaultLanguage="python"
+                defaultValue="// Write your Code Here"
+            />
         </div>
     );
 }
