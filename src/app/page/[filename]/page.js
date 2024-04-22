ac7bc43497b2db9fa831cc1bd5949ce6d3761311
Gaurav Dhyani <gauravdhyani90@gmail.com>
2024-04-22 16:33:33 +0530

    Editor theme black board

--- src/app/page/[filename]/page.js
+++ src/app/page/[filename]/page.js
@@ -1,12 +1,28 @@
 "use client";
 import { useRouter } from "next/navigation";
-import Editor from "@monaco-editor/react";
+import Editor, { DiffEditor, useMonaco, loader } from "@monaco-editor/react";
+import "monaco-themes/themes/Blackboard.json";
+import { useEffect } from "react";
+
 export default function Filepage({ params }) {
     const router = useRouter();
+    const monaco = useMonaco();
+    useEffect(() => {
+        if (monaco) {
+          console.log("here is the monaco isntance:", monaco);
+          import('monaco-themes/themes/Blackboard.json')
+            .then(data => {
+              monaco.editor.defineTheme('blackboard', data);
+            })
+            .then(_ => monaco.editor.setTheme('blackboard'))
+          // monaco.editor.defineTheme("monokai-bright").then(_ => monaco.editor.setMonacoTheme("monokai-bright"));
+        }
+      }, [monaco]);
     return (
         <div className="h-screen">
             <h1 className="border text-muted-foreground">{params.filename}</h1>
             <Editor
+                className="bg-black"
                 theme="vs-dark"
                 height="90vh"
                 defaultLanguage="python"
