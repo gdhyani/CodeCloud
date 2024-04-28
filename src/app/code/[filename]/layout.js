795d5b59bcd8045d993aa0e89b329f27e631d6f6
Gaurav Dhyani <gauravdhyani90@gmail.com>
2024-04-28 19:44:27 +0530

    new Layout Setting

---
+++ src/app/code/[filename]/layout.js
@@ -0,0 +1,77 @@
+"use client";
+import Image from "next/image";
+import {
+    ResizableHandle,
+    ResizablePanel,
+    ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { ArrowUpFromLine, ArrowDownFromLine } from "lucide-react";
+import { useEffect, useState, useRef } from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+export default function FileLayout({children}) {
+    // const [console, setconsole] = useState(false);
+
+    function handleTabChange() {
+        alert("change");
+    }
+    return (
+        <main className="h-full">
+            <ResizablePanelGroup
+                direction="vertical"
+                className=" rounded-lg border"
+            >
+                <ResizablePanel className="" defaultSize={75} maxSize={93}>
+                    {/* <div className="flex flex-col gap-3 text-center h-full overflow-y-auto items-center justify-center">
+                        <h1 className="text-4xl">
+                            Welcome to{" "}
+                            <span className="font-bold">CodeCloud🔥</span>
+                        </h1>
+                        <p className="text-lg text-muted-foreground">
+                            Lets start your new Project.
+                        </p>
+                    </div> */}
+                    {children}
+                </ResizablePanel>
+                <ResizableHandle />
+                <ResizablePanel
+                    collapsible
+                    defaultSize={25}
+                    minSize={5.2}
+                    maxSize={60}
+                >
+                    <div className="flex px-0">
+                        <Tabs defaultValue="none" className="w-[400px]">
+                            <TabsList className="bg-transparent font-extralight">
+                                <TabsTrigger className="font-semibold text-muted-foreground" value="problem">
+                                    PROBLEMS
+                                </TabsTrigger>
+                                <TabsTrigger className="font-semibold text-muted-foreground" value="output">OUTPUT</TabsTrigger>
+                                <TabsTrigger className="font-semibold text-muted-foreground" value="terminal">TERMINAL</TabsTrigger>
+                                <TabsTrigger className="font-semibold text-muted-foreground" value="console">DEBUG CONSOLE</TabsTrigger>
+                            </TabsList>
+                            <hr className="w-screen -ml-10" />
+                            <TabsContent value="console">
+                                Console.log your data.
+                            </TabsContent>
+                            <TabsContent value="github">
+                                GitHub Logs Here.
+                            </TabsContent>
+                        </Tabs>
+                    </div>
+                </ResizablePanel>
+            </ResizablePanelGroup>
+        </main>
+    );
+}
+
+
+
+
+// export default function Page() {
+//     return (
+//         <div>
+//             <h1>Pages on Demand</h1>
+//         </div>
+//     );
+// }
--- src/app/code/page.js
+++ src/app/code/page.js
@@ -1,7 +1,21 @@
+import { ArrowBigRight, LucideArrowRight } from "lucide-react";
+
 export default function Page() {
     return (
-        <div>
-            <h1>Pages on Demand</h1>
+        <div className="h-screen">
+            <div className=" flex flex-col gap-3 text-center h-full overflow-y-auto items-center justify-center">
+                <h1 className="text-4xl">
+                    Welcome to <span className="font-bold">CodeCloud🔥</span>
+                </h1>
+                <p className="text-lg text-muted-foreground">
+                    Lets start your new Project.
+                </p>
+
+                <button className="mt-4 border gap-2 px-2 py-1 rounded-xl items-center flex flex-row">
+                    Lets Build a <span className="font-black font-mono">New Project</span>
+                    <LucideArrowRight size={16}/>
+                </button>
+            </div>
         </div>
     );
 }
