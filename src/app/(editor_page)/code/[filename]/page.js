af04a548dd356388ff20ae7405d391908f9afd78
Gaurav Dhyani <gauravdhyani90@gmail.com>
2024-05-03 23:42:34 +0530

    file page changes

--- src/app/(editor_page)/code/[filename]/page.js
+++ src/app/(editor_page)/code/[filename]/page.js
@@ -4,21 +4,26 @@ import AceEditor from "react-ace";
 import "ace-builds/src-noconflict/mode-javascript";
 import "ace-builds/src-noconflict/theme-twilight";
 import "ace-builds/src-noconflict/ext-language_tools";
+import { useState } from "react";
 
 export default function Filepage({ params }) {
+  const [code, setCode] = useState("")
     function onChange(newValue) {
-        console.log("change", newValue);
+        // console.log("change", newValue);
+        setCode(newValue)
     }
     return (
         <div className="h-screen">
             <h1 className="border text-muted-foreground">{params.filename}</h1>
+            <button className="bg-white text-black" onClick={()=>{console.log(code)}}>get started</button>
             <AceEditor
                 mode="javascript"
                 theme="twilight"
                 height="90vh"
-                width="100%"
+                width="screen"
                 fontSize={16}
                 onChange={onChange}
+                value={code}
                 name="UNIQUE_ID_OF_DIV"
                 lineHeight={28}
                 editorProps={{ $blockScrolling: true }}
