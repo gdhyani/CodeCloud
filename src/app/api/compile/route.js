9dd7c0e4c7849176b87b2637a31d8006ace977f1
Gaurav Dhyani <gauravdhyani90@gmail.com>
2024-05-06 14:29:25 +0530

    api testing 2

--- src/app/api/compile/route.js
+++ src/app/api/compile/route.js
@@ -1,45 +1,31 @@
-const { NextResponse } = require("next/server");
+import { NextResponse } from "next/server";
 
 export async function GET() {
-    const response = await fetch(
-        "https://api.freeapi.app/api/v1/todos?query=reactjs&complete=false",
-        {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: {
-                password: "test@123",
-                username: "doejohn",
-            },
-        }
-    );
-    const res = await response.json();
-    console.log(res, response);
-    return NextResponse.json(res);
-}
-
-export async function POST(request) {
     const url = "https://onecompiler-apis.p.rapidapi.com/api/v1/run";
     const options = {
         method: "POST",
         headers: {
             "content-type": "application/json",
-            
+            "X-RapidAPI-Key":process.env.X_RapidAPI_Key,
+            "X-RapidAPI-Host":process.env.X_RapidAPI_Host,
         },
-        body: {
+        body: JSON.stringify({
             language: "javascript",
-            stdin: "Peter",
             files: [
                 {
-                    name: "index.py",
-                    content: "console.log('codecloud')",
+                    name: "index.js",
+                    content: "console.log(23+23*2)",
                 },
             ],
-        },
+        }),
     };
-    const response = await fetch(url, options);
-	const result = await response.text();
-    // const data = await request.json();
-    return NextResponse.json(result);
+    try {
+        const res = await fetch(url, options);
+        const data = await res.json();
+        return NextResponse.json(data);
+        // console.log(data);
+    } catch (error) {
+        console.error(error);
+        // return NextResponse.error(error);
+    }
 }
