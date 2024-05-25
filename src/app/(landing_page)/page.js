c51d2038e99b728b2406f050db51a141eaca7734
Gaurav Dhyani <gauravdhyani90@gmail.com>
2024-05-25 23:51:29 +0530

    Added section on landing page

--- src/app/(landing_page)/page.js
+++ src/app/(landing_page)/page.js
@@ -5,12 +5,7 @@ export default function Home() {
     return (
         <main className="dark h-full">
             <Mainherosection />
-            {/* <Image className="border-4 border-[#212121]" src="/hero.jpg" alt="hero" width={1000} height={1000}/> */}
-            {/* <div className="min-h-screen md:border-t-2 border-slate-400">
-                <h1 className="text-3xl md:text-6xl text-center font-semibold md:py-20 py-5">
-                    Features
-                </h1>
-            </div> */}
+            <section className="h-screen"></section>
         </main>
     );
 }
